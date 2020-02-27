0f0d41c7ef44de608af2c61cfbca2844c0c24af7
doanphu90 <doanphu90@gmail.com>
2020-02-27 13:37:57 +0700

    check in
    commit

--- app/src/main/java/com/kotlin/spweather_app/Interface/IOnClickListenItem.kt
+++
@@ -1,7 +0,0 @@
-package com.kotlin.spweather_app.Interface
-
-import com.kotlin.spweather_app.model.ResultInfo
-
-interface IOnClickListenItem {
-    fun onClickItemListener(item: ResultInfo, position:Int)
-}
--- app/src/main/java/com/kotlin/spweather_app/api/BaseResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.kotlin.spweather_app.api
-
-data class BaseResponse (var title: String? = null,
-                         var message: String? = null,
-                         var request_code: String? = null)
